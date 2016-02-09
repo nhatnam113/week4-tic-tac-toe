2e55a938de88ca22d0d0999ee2985ee4df19cb38
nhatnam113 <nnguyenpham@mail.valenciacollege.edu>
2016-02-08 19:22:09 -0800

    w4

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,10 @@
 def printBoard(board):
+    print(board['top-L'] + board['top-M] + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + board['mid-M'] + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + board['low-M'] + board['low-R'])
+
     # TO DO #################################################################
     # Write code in this function that prints the game board.               #
     # The code in this function should only print, the user should NOT      #
@@ -9,6 +15,8 @@ def printBoard(board):
 
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
+    
+    return (( board['top-L']== player and board['top-M']==         player and board['top-R']== player or board['mid-L']== player and board['mid-M']== player and board['mid-R']== player or board['low-L']== player and board['low-M']== player and board['low-R']== player or board['top-L']== player and board['mid-L']== player and board['low-L']== player or board['top-M']== player and board['mid-M']== player and board['low-M']== player or board['top-R']== player and board['mid-R']== player or board['low-R']== player or board['top-L']== player and board['mid-M']= player and board['low-R']== player or board['top-R']== player and board['mid-M']== player and board['low-L']== player))
     
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
